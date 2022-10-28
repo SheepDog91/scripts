2502d8d6961a575697f3b193945474bbb1f3bae5
SheepDog91 <93393002+SheepDog91@users.noreply.github.com>
2022-10-27 23:57:22 -0500

    Create restore_dotfiles.sh

---
+++ restore_dotfiles.sh
@@ -0,0 +1,24 @@
+#! usr/bin/bash
+
+git clone https://github.com/sheepdog91/dotfiles
+
+# /etc/
+
+        # MOTD
+cp      dotfiles/etc/motd /etc/ &&
+
+        # Apt Sources
+cp      dotfiles/etc/apt/sources.list /etc/apt/ &&
+
+        # Default Console Setup
+cp      dotfiles/etc/default/console-setup /etc/default/ &&
+
+# /home/
+    
+cp      dotfiles/home/xinitrc ~/.xinitrc &&
+    
+# /home/.config
+    
+cp -r   dotfiles/home/config/       ~/.config/nvim/ &&
+cp -r   dotfiles/home/config/       ~/.config/i3/ &&
+cp -r   dotfiles/home/config/       ~/.config/i3status/ &&
