c8c050962d741df049a0c4668ac3f78d60d85301
SheepDog91 <93393002+SheepDog91@users.noreply.github.com>
2022-10-28 15:48:27 -0500

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -52,7 +52,7 @@ apt install neovim &&
 apt install     libsecret-1-0 gconf2 gconf-service libgtk2.0-0 xdg-utils
     wget https://github.com/minbrowser/min/releases/download/v1.26.0/min-1.26.0-amd64.deb
     dpkg -i min-1.26.0-amd64.deb
-    rm min-1.26.0-amd64.deb
+    rm min-1.26.0-amd64.deb\
     &&
     
     # Terminal Applications
