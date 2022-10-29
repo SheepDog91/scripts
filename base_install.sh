7772fa1e4baf0dc2761b7218e2bb62e1b30f8159
SheepDog91 <93393002+SheepDog91@users.noreply.github.com>
2022-10-28 20:06:15 -0500

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -13,59 +13,71 @@ rm sources.list
 
 # Update repository sources and check for updates
 
-apt update &&
-apt upgrade &&
+apt update && apt upgrade &&
+
+# SUDO
+
+apt install     sudo && adduser sheepdog sudo &&
 
 # Install and set my prefered shell
 
-apt install fish &&
-chsh -s /usr/bin/fish root
-chsh -s /usr/bin/fish sheepdog
+apt install fish && chsh -s /usr/bin/fish root && chsh -s /usr/bin/fish sheepdog
 
-# Install my prefered text editor
+# TextEditor
 
 apt install neovim &&
 
-# My Desktop Enviroment
-
-    # Base
-
-    apt install xserver-xorg-core\
-                xserver-xorg-video-amdgpu\
-                xserver-xorg-input-evdev\
-                x11-xserver-utils\
-                x11-xkb-utils\
-                x11-utils\
-                xinit\
-                i3\
-                feh\
-                compton\
-                rxvt-unicode\
-                alsa-utils\
-                pulseaudio\
-                pulseaudio-utils\
-                cups\
-                bluez\
-    &&
-
-    # GUI Applications
-apt install     libsecret-1-0 gconf2 gconf-service libgtk2.0-0 xdg-utils
-    wget https://github.com/minbrowser/min/releases/download/v1.26.0/min-1.26.0-amd64.deb
-    dpkg -i min-1.26.0-amd64.deb
-    rm min-1.26.0-amd64.deb\
-    &&
-    
-    # Terminal Applications
-
-    apt install neofetch\
-                htop\
-    &&
 # Fun Nerd Shit
 
 apt install     docker\
                 git\
                 ssh\
-                sudo\
 &&
-adduser sheepdog sudo
+
+# Audio files
+
+apt install alsa-utils\
+            pulseaudio\
+            pulseaudio-utils\
+&&
+
+# Window Manager
+
+apt install i3\
+&&
+
+# Image Viewer
+
+apt install feh\
+&&
+
+# Compositor
+
+apt install picom\
+&&
+
+# Terminal Emulator
+apt install kitty\
+&&
+
+# X.Org Minimum Install
+
+apt install xserver-xorg-core\
+            xserver-xorg-video-amdgpu\
+            xserver-xorg-input-evdev\
+            x11-xserver-utils\
+            x11-xkb-utils\
+            x11-utils\
+            xinit\
+&&
+
+apt install cups &&
+apt install bluez &&
+
+# GUI Applications
+apt install libsecret-1-0 gconf2 gconf-service libgtk2.0-0 xdg-utils
+wget    https://github.com/minbrowser/min/releases/download/v1.26.0/min-1.26.0-amd64.deb
+dpkg -i min-1.26.0-amd64.deb
+rm      min-1.26.0-amd64.deb\
+&&
 reboot
